2835eaf5d94592a920688fc09cb48bed0135ff34
Raine Revere <raine@cybersemics.org>
2021-05-09 10:33:46 -0600

    test

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,25 @@
-test('print markdown', () => {
-  expect(true).toBe(false)
+import execa from 'execa'
+
+test('print markdown', async () => {
+  const { stdout } = await execa('node', ['./src/cli.js', 'README.md'])
+  expect(stdout).toBe(`# marked-terminal-cli
+
+![npm version](https://img.shields.io/npm/v/marked-terminal-cli.svg)
+ (https://npmjs.org/package/marked-terminal-cli)
+
+A better way to read README's in the CLI!
+
+Read and print markdown files using marked-terminal (https://github.com/mikaelbr/marked-terminal).
+
+## Install
+
+    npm install -g marked-terminal-cli
+
+## Usage
+
+    marked-terminal-cli README.md
+
+You may need to force color when piping to less:
+
+    FORCE_COLOR=1 marked-terminal-cli README.md | less -r`)
 })
